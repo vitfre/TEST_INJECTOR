ee372bb2541e76bfdbb210dea63a02f1b95a9bc3
vitfre <vitfre@ukr.net>
2015-09-16 14:33:00 +0300

    Remap_inputs

--- Embedded/UControl_Module_M64A/inputs.h
+++ Embedded/UControl_Module_M64A/inputs.h
@@ -15,12 +15,16 @@
 #define GET_Val_IN_3       ((PINE)>>(5)
 #define GET_Val_IN_4       ((PINE)>>(6)
 #define GET_Val_IN_5       ((PINE)>>(7)
+
 #define GET_Val_IN_6       ((PINB)>>(0)
 #define GET_Val_IN_7       ((PINB)>>(1)
 #define GET_Val_IN_8       ((PINB)>>(2)
-#define GET_Val_IN_9       ((PIND)>>(6)
-#define GET_Val_IN_10      ((PINA)>>(1)
-#define GET_Val_IN_11      ((PIND)>>(4)
-#define GET_Val_IN_12      ((PINF)>>(5)
 
+#define GET_Val_IN_9       ((PINF)>>(5)
+
+#define GET_Val_IN_10      ((PIND)>>(4)
+
+#define GET_Val_IN_11      ((PINA)>>(1)
+
+#define GET_Val_IN_12      ((PIND)>>(6)
 #endif /* INPUTS_H_ */
